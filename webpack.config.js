0290b08149dcb4e1d2033309b246b8027493089a
avwo <avwu@qq.com>
2017-08-31 15:50:04 +0800

    chore: eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 var webpack = require('webpack');
-var path = require('path');
 
 module.exports = {
-	entry : {
-		index : './src/js/index'
-	},
-	output : {
-		path : './app/public/js',
-		filename : '[name].js'
-	},
-	module : {
-		loaders : [ {
-			test : /\.js$/,
-			exclude : /node_modules/,
-			loader : 'jsx-loader?harmony'
-		}, {
-			test : /\.css$/,
-			loader : 'style-loader!css-loader'
-		}, {
-			test : /\.(png|woff|woff2|eot|ttf|svg)$/,
-			loader : 'url-loader?limit=1000000'
-		} ]
-	},
-	plugins : [new webpack.optimize.UglifyJsPlugin({
-	    compress: {
-	        warnings: false
-	    }
-	})]
+  entry : {
+    index : './src/js/index'
+  },
+  output : {
+    path : './app/public/js',
+    filename : '[name].js'
+  },
+  module : {
+    loaders : [ {
+      test : /\.js$/,
+      exclude : /node_modules/,
+      loader : 'jsx-loader?harmony'
+    }, {
+      test : /\.css$/,
+      loader : 'style-loader!css-loader'
+    }, {
+      test : /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader : 'url-loader?limit=1000000'
+    } ]
+  },
+  plugins : [new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })]
 };
